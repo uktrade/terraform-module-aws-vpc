8b0d6482e48c90a2bfe4e1ed96f569d575a05ee9
Bowen Sun <bowensun@gmail.com>
2017-03-17 15:57:57 +0000

    * limit ssh access from VPC network

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_security_group" "base-sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.aws_conf["cidr_block"]}"]
   }
 
   tags {
