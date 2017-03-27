de5dd034a96751d2d666934f0ff697a57976c707
Bowen Sun <bowensun@gmail.com>
2017-03-27 10:59:12 +0100

    * fix egress security group rule

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "base-sg" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["${var.aws_conf["cidr_block"]}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
