e906e62f75c2c85b06df243f6ec281128686d2e1
Bowen Sun <bowensun@gmail.com>
2017-07-17 11:23:38 +0100

    * fix aws_vpc_peering_connection data resource

--- peering.tf
+++ peering.tf
@@ -4,9 +4,12 @@ provider "aws" {
 }
 
 data "aws_vpc_peering_connection" "peer" {
-  vpc_id = "${aws_vpc.default.id}"
-  peer_vpc_id = "${var.aws_conf["peering.vpc_id"]}"
-  peer_owner_id = "${var.aws_conf["peering.account_id"]}"
+  vpc_id = "${var.aws_conf["peering.vpc_id"]}"
+  owner_id = "${var.aws_conf["peering.account_id"]}"
+  cidr_block = "${var.aws_conf["peering.cidr_block"]}"
+  peer_vpc_id = "${aws_vpc.default.id}"
+  peer_owner_id = "${var.aws_conf["account_id"]}"
+  peer_cidr_block = "${var.aws_conf["cidr_block"]}"
 }
 
 resource "aws_vpc_peering_connection_accepter" "peer" {
