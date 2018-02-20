4e85d9dd70739d2fe51dededb7e6134aa4f2359f
Bowen Sun <bowensun@gmail.com>
2018-02-20 17:18:42 +0000

    use internal DNS for VPC

--- dns.tf
+++ dns.tf
@@ -1,5 +1,6 @@
 resource "aws_route53_zone" "root_zone" {
   name = "${var.aws_conf["domain"]}."
+  vpc_id = "${aws_vpc.default.id}"
   tags {
     Name = "${var.aws_conf["domain"]}"
     Stack = "${var.aws_conf["domain"]}"
